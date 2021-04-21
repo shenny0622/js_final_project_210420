b404bddc49e7b25123392dea37b7f42d069fd930
shenny0622 <65540548+shenny0622@users.noreply.github.com>
2021-04-21 23:38:09 +0800

    Update admin.js
    delet all

--- js/admin.js
+++ js/admin.js
@@ -227,4 +227,17 @@ function deleteOrderItem(id){
 
 }
 
-
+// 刪除後台全部訂單
+const discardAllBtn = document.querySelector(".discardAllBtn");
+discardAllBtn.addEventListener("click",function(e){
+  e.preventDefault();
+  axios.delete(`https://hexschoollivejs.herokuapp.com/api/livejs/v1/admin/${api_path}/orders`, {
+    headers: {
+      'Authorization': token,
+    }
+  })
+    .then(function (response) {
+      alert("刪除全部訂單成功");
+      getOrderList();
+    })
+})
